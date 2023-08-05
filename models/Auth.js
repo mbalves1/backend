7b6302578e0c9a6130ac4e05b369154d99c499ee
Murilo <mbalves1@outlook.com>
2023-08-05 12:06:28 -0300

    [feat]: register and login created

---
+++ models/Auth.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+
+const { Schema } = mongoose
+
+const userSchema = new Schema({
+  first_name: String,
+  last_name: String,
+  email: String,
+  password: String,
+},
+{ timestamps: true }
+)
+
+const User = mongoose.model("User", userSchema)
+
+module.exports = {
+  User,
+  userSchema,
+}
